174697866c3c657e0c0445a941acfdce69db1d69
SergeyPodgorny <serkonpod@gmail.com>
2023-11-11 12:28:21 +0300

    initial commit

---
+++ src/main/java/ru/jenkins/sandbox/JenkinsSandboxApplication.java
@@ -0,0 +1,13 @@
+package ru.jenkins.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSandboxApplication.class, args);
+	}
+
+}
