5bf6601914bcf75452ce31eab49d7783442e06c6
SergeyPodgorny <serkonpod@gmail.com>
2023-11-11 12:35:34 +0300

    added basic endpoint

--- src/main/java/ru/jenkins/sandbox/JenkinsSandboxApplication.java
+++ src/main/java/ru/jenkins/sandbox/JenkinsSandboxApplication.java
@@ -2,12 +2,21 @@ package ru.jenkins.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsSandboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsSandboxApplication.class, args);
+	}
+
+
+	@GetMapping("/start")
+	public String showMessage(){
+		return "message";
 	}
 
 }
